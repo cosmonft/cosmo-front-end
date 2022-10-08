9a1b23a03038d4b741b1a20b98995cbb8efc2718
gpablo6 <gpablo6@outlook.com>
2022-10-08 15:25:11 -0500

    Add web3reactprovider component to dapp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Web3ReactProvider } from "@web3-react/core";
+import { providers } from "ethers";
 
 import './App.css';
 
@@ -8,9 +10,14 @@ import { Home } from './home';
 import { Layout } from './Layout';
 import { Gallery } from './gallery';
 
+function getLibrary(provider) {
+  return new providers.Web3Provider(provider);
+}
+
 function App() {
   return (
     <React.Fragment>
+      <Web3ReactProvider getLibrary={getLibrary}>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
@@ -20,6 +27,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      </Web3ReactProvider>
     </React.Fragment>
   );
 }
